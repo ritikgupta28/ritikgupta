782b1dfafed0c0567c133e95f73e51f54b52bf08
Ritik Gupta <ritikgupta2003@gmail.com>
2020-10-17 03:03:40 +0530

    portfolio done

---
+++ src/Components/Footer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    if(this.props.data) {
+      var { social } = this.props.data;
+      var networks = social.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <footer>
+      <div className="row">
+        <div className="twelve columns">
+           <ul className="social-links">
+              {networks}
+           </ul>
+           <ul>
+            <li>Made with Love <span role="img" aria-label="love">❤️</span></li>
+           </ul>
+           <ul className="copyright">
+              <li>&copy; Copyright 2020</li>
+           </ul>
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+     </div>
+     </footer>
+    );
+  }
+}
+
+export default Footer;
